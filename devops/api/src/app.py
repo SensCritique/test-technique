f0073c5bc1d2e35fafa89cbaa0e0cb42ba8a1cb9
pkrikorian <pierre.krikorian@discngine.com>
2023-02-01 22:33:04 +0100

    exo 3

--- devops/api/src/app.py
+++ devops/api/src/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import json
+from flask_caching import Cache
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+cache = Cache(app)
 
 fichier = open("/home/books.json", "r")
 books = json.load(fichier)
 
+@cache.cached(timeout=30, query_string=True)
 @app.route("/book/<identifient>", methods=['POST', 'GET'])
 def get_book_par_ident(identifient):
     for book in books:
